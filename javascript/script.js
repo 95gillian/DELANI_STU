b29e821fea463f2ac18299b1a6ab82c36a841ae4
Gillian Nyangaga <gillian.nyangaga@moringaschool.com>
2022-03-14 11:56:54 +0300

    newest portfolio edit;added hover effect

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   $("#design").click(function(){
-    $("#design-showing").toggle();
+    $("#design").toggle();
     $(".hide-design-image").toggle();
   })
 
-  $("#development").click(function(){
-    $("#development-showing").toggle();
-    $(".hide-development-image").toggle();
+  $("#dev").click(function(){
+    $("#dev").toggle();
+    $(".hide-image").toggle();
   })
 
-  $("#productManagement").click(function(){
-    $("#product-management-showing").toggle();
-    $(".hide-product-management").toggle();
+  $("#prod").click(function(){
+    $("#prod").toggle();
+    $(".hide-image").toggle();
   })
 
 
