07eeaefedf4407dc500289edc77f1577a21a619a
Gillian Nyangaga <gillian.nyangaga@moringaschool.com>
2022-03-14 13:38:09 +0300

    edit whatwedo section

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,34 @@
-$(document).ready(function() {
-  $("#design").click(function(){
-    $("#design").toggle();
-    $(".hide-design-image").toggle();
-  })
 
-  $("#dev").click(function(){
-    $("#dev").toggle();
-    $(".hide-image").toggle();
-  })
+  $(document).ready(function() {
 
-  $("#prod").click(function(){
-    $("#prod").toggle();
-    $(".hide-image").toggle();
-  })
-
-
+    $("#design").click(function(){
+      $("#design-showing").toggle();
+      $(".hide-design-image").toggle();
+    })
+  
+    $("#development").click(function(){
+      $("#development-showing").toggle();
+      $(".hide-dev-image").toggle();
+    })
+  
+    $("#productManagement").click(function(){
+      $("#product-management-showing").toggle();
+      $(".hide-prod-image").toggle();
+    })
+  
+  
+  $('.submit').click(function () {
+          var Name = $('#mce-FNAME').val();
+          var Email = $('#mce-EMAIL').val();
+          var Message = $('#mce-MESSAGE');
+          var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+          if (Name == '' || Email == '' || Message == '') {
+              alert('Please make sure you have filled in the form correctly!');
+          } else {
+              alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+          }
+    });
+  });
   
 
-$('.submit').click(function () {
-        var Name = $("#FNAME name").val();
-        var Email = $("#EMAIL").val();
-        var Message = $("#MESSAGE");
-        var key ="c8fbd9a0cf3958e02be2822e55cfbfc5-us4";
-        if (Name == '' || Email == '' || Message == '') {
-            alert("Please make sure you have filled in the form correctly!");
-        } else {
-            alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
-        }
-  });
-});
+  
